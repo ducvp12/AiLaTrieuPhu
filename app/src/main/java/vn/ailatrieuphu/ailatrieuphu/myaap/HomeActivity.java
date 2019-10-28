f937a94a95cf8800dec9633e852e7e1bd49825b3
ducvp12 <ducdvph06151@fpt.edu.vn>
2019-10-28 13:53:09 +0700

    Đã xong đang nhập  và google nhưng chỉ lấy được dự liệu từ google. FB mình chỉ được dùng phiên bản thử nghiệm nên chỉ đăng nhập được bằng 1 acc duy nhất nên rất có thể sẽ xóa login bằng fb

--- app/src/main/java/vn/ailatrieuphu/ailatrieuphu/myaap/HomeActivity.java
+++ app/src/main/java/vn/ailatrieuphu/ailatrieuphu/myaap/HomeActivity.java
@@ -1,21 +1,44 @@
 package vn.ailatrieuphu.ailatrieuphu.myaap;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ImageView bg;
+    private ImageView btnDangnhap;
+    private ImageView btnDangky;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_active);
+        initView();
+        btnDangky.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, DangNhapActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnDangnhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, HomeActiveActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void Dangnhap(View view) {
-        Intent intent=new Intent(HomeActivity.this, DangNhapActivity.class);
-        startActivity(intent);
+
+
+    private void initView() {
+        bg = (ImageView) findViewById(R.id.bg);
+        btnDangnhap = (ImageView) findViewById(R.id.btn_choingay);
+        btnDangky = (ImageView) findViewById(R.id.btn_dangky);
     }
 }
