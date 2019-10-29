872e327352bc8f1d7843f14247f72466358593d2
duynam62 <duynam6262@gmail.com>
2019-10-29 17:05:20 +0700

    animation

--- app/src/main/java/vn/ailatrieuphu/ailatrieuphu/myaap/ui/home/HomeFragment.java
+++ app/src/main/java/vn/ailatrieuphu/ailatrieuphu/myaap/ui/home/HomeFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -18,11 +21,13 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageView imageView = root.findViewById(R.id.img_launcher);
+        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.rote);
+        imageView.startAnimation(animation);
 
         return root;
     }
